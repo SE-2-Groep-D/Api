afccb66f0ca93b4740686c3c0469f9a1275ff7ef
David Out <david.out@migrato.nl>
2023-12-08 17:06:39 +0100

    Connect backend with frontend

---
+++ api/Program.cs
@@ -0,0 +1,35 @@
+namespace AccessibilityPanelAPI; 
+public class Program { 
+    public static void Main(string[] args) {
+            // configure application
+            var builder = WebApplication.CreateBuilder(args);
+            SetupServices(builder.Services);
+            
+            // add middelware
+            var app = builder.Build();
+            SetupMiddleware(app);
+            
+            // run application
+            app.Run();
+    }
+
+    private static void SetupServices(IServiceCollection services) { 
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+    private static void SetupMiddleware(WebApplication app) {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment()) {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+        
+        app.MapControllers();
+    }
+}
