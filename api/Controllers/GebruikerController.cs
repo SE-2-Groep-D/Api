f4674ec6b18a4146f0dbbef4f8b04f5471498a35
David Out <david.out@migrato.nl>
2023-12-13 17:18:51 +0100

    GebruikerController routes uitgewerkt

--- api/Controllers/GebruikerController.cs
+++ api/Controllers/GebruikerController.cs
@@ -1,18 +1,95 @@
 ﻿using Api.Models.Domain;
+using API.Models.DTO.Gebruiker;
 using Api.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers; 
 
+[Route("[controller]")]
+[ApiController]
 public class GebruikerController : ControllerBase {
 
     private UserManager<Gebruiker> _userManager;
-    private IGebruikerRepository _gebruikerRepository;
     
-    public GebruikerController(UserManager<Gebruiker> manager, IGebruikerRepository repository) {
+    public GebruikerController(UserManager<Gebruiker> manager) {
         this._userManager = manager;
-        this._gebruikerRepository = repository;
     }
     
+    [HttpGet]
+    [Route("list")]
+    public async Task<IActionResult> GetAll() {
+        var users = await _userManager.Users.ToListAsync();
+
+        if (users.Count == 0) {
+            return NotFound("Geen gebruikers gevonden.");
+        }
+
+        return Ok(users);
+    }
+    
+    [HttpGet]
+    [Route("{email}")]
+    public async Task<IActionResult> GetByEmail([FromRoute] string email) {
+        Gebruiker? user = await _userManager.FindByEmailAsync(email);
+
+        if (user == null) {
+            return NotFound("Gebruiker niet gevonden.");
+        }
+        
+        return Ok(user);
+    }
+    
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] string id) {
+        Gebruiker? user = await _userManager.FindByIdAsync(id);
+
+        if (user == null) {
+            return NotFound("Gebruiker niet gevonden.");
+        }
+        
+        return Ok(user);
+    }
+
+
+    [HttpPut]
+    [Route("{id}/update")]
+    public async Task<IActionResult> Update([FromRoute] string id, [FromBody] UpdateGebruikerRequestDto request) {
+        Gebruiker? user = await _userManager.FindByIdAsync(id);
+
+        if (user == null) {
+            return NotFound("Gebruiker niet gevonden om te updaten.");
+        }
+
+        user.Email = request.Email ?? user.Email;
+        user.Voornaam = request.Voornaam ?? user.Voornaam;
+        user.Achternaam = request.Achternaam ?? user.Achternaam;
+        
+        var result = await _userManager.UpdateAsync(user);
+        if(!result.Succeeded) return BadRequest("Kon gebruiker niet updaten.");
+
+        if (request.Roles != null) {
+            var identityResult = await _userManager.AddToRolesAsync(user, request.Roles);
+            if (!identityResult.Succeeded) {
+                return BadRequest("Kon rollen niet toevoegen.");
+            }
+        }
+        
+        return Ok("Gebruiker succesvol geupdate.");
+    }
+
+    [HttpDelete]
+    [Route("{id}/delete")]
+    public async Task<IActionResult> Delete([FromRoute] string id) {
+        Gebruiker? user = await _userManager.FindByIdAsync(id);
+
+        if (user != null) {
+            var result = await _userManager.DeleteAsync(user);
+            return BadRequest("Kon gebruiker niet verwijderen.");
+        }
+       
+        return Ok("Gebruiker succesvol verwijderd.");
+    }
 }
