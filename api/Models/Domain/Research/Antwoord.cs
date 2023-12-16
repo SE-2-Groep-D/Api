b72a993b37749fabe037e6f7434003d425a557f7
Ozancann01 <22008306@student.hhs.nl>
2023-12-16 17:07:04 +0100

    onderzoek class
    oderzoek en bijhorden classses toegevoeg.

---
+++ api/Models/Domain/Research/Antwoord.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Models.Domain.Research; 
+public class Antwoord {
+
+  public Guid Id { get; set; }
+  public string Tekst { get; set; }
+  public Guid VraagtId { get; set; }
+  public Vraag Vraag { get; set; }
+}
---
+++ api/Models/Domain/Research/Vraag.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models.Domain.Research; 
+public class Vraag {
+
+  public Guid Id{ get; set; }
+  public string Type{ get; set; }
+  public string Onderwerp { get; set; }
+  public Guid VragenlijstId { get; set; }
+  public Vragenlijst Vragenlijst { get; set; }
+  public ICollection<Antwoord> Antwoorden { get; } = new List<Antwoord>();
+
+}
