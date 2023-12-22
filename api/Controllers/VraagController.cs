c9c0a35e3291a3bf47d829adc49a25b8a67f98d9
Ozancann01 <22008306@student.hhs.nl>
2023-12-22 14:12:48 +0100

    code is reformated

--- api/Controllers/VraagController.cs
+++ api/Controllers/VraagController.cs
@@ -5,80 +5,69 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
-
 [Route("[controller]")]
 [ApiController]
-public class VraagController : ControllerBase  {
+public class VraagController : ControllerBase {
 
   private IVraagRepository _vraagRepository;
   private IMapper _mapper;
-  
-  
-  public VraagController(IMapper _mapper,IVraagRepository _vragenlijstRepository) {
 
+
+  public VraagController(IMapper _mapper, IVraagRepository _vragenlijstRepository) {
     this._mapper = _mapper;
     this._vraagRepository = _vragenlijstRepository;
-
   }
-  
+
   [HttpGet]
   [Route("list")]
   public async Task<ActionResult> GetAllVragen(Guid VragenLijstId) {
     var vragenlijst = await _vraagRepository.GetAllAsync(VragenLijstId);
     var vragenlijstDtos = _mapper.Map<IEnumerable<VraagDto>>(vragenlijst);
     return Ok(vragenlijstDtos);
-
   }
-  
+
   [HttpGet]
-  [Route("{id}")] 
-  public async Task<ActionResult> GetById(Guid id)
-  {
+  [Route("{id}")]
+  public async Task<ActionResult> GetById(Guid id) {
     var vraag = await _vraagRepository.GetByIdAsync(id);
-    if (vraag == null)
-    {
-      return NotFound("vraag is niet gevonden met de "+id);
+    if (vraag == null) {
+      return NotFound("vraag is niet gevonden met de " + id);
     }
+
     var vraagDto = _mapper.Map<VraagDto>(vraag);
     return Ok(vraagDto);
   }
-  
+
   [HttpPost]
   [Route("create")]
-  public async Task<ActionResult<VraagDto>> Create([FromBody] AddVraagRequestDto addDto) 
-  {
+  public async Task<ActionResult<VraagDto>> Create([FromBody] AddVraagRequestDto addDto) {
     var vraag = _mapper.Map<Vraag>(addDto);
     var nieuwVraag = await _vraagRepository.CreateAsync(vraag);
     var nieuwVraagDto = _mapper.Map<VraagDto>(nieuwVraag);
     return CreatedAtAction(nameof(GetById), new { id = nieuwVraagDto.Id }, nieuwVraagDto);
   }
-  
+
   [HttpPut]
   [Route("update/{id}")]
-  public async Task<IActionResult> Update(Guid id, [FromBody] UpdateVraagRequestDto request)
-  {
-    try
-    {
+  public async Task<IActionResult> Update(Guid id, [FromBody] UpdateVraagRequestDto request) {
+    try {
       Vraag? bestaandVraag = await _vraagRepository.GetByIdAsync(id);
 
-      if (bestaandVraag == null)
-      {
+      if (bestaandVraag == null) {
         return NotFound($"Vraag met ID {id} is niet gevonden.");
       }
 
       _mapper.Map(request, bestaandVraag);
 
-      Vraag? isUpdated = await _vraagRepository.UpdateAsync(id,bestaandVraag);
+      Vraag? isUpdated = await _vraagRepository.UpdateAsync(id, bestaandVraag);
 
-      if (isUpdated==null)
-      {
-        return StatusCode(StatusCodes.Status500InternalServerError, "Er is een fout opgetreden bij het bijwerken van het vraag.");
+      if (isUpdated == null) {
+        return StatusCode(StatusCodes.Status500InternalServerError,
+          "Er is een fout opgetreden bij het bijwerken van het vraag.");
       }
 
       return Ok(isUpdated);
-    }
-    catch (Exception ex)
-    {
+    } catch (Exception ex) {
       var errorMsg = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
 
       return StatusCode(StatusCodes.Status500InternalServerError, $"Interne serverfout: {errorMsg}");
@@ -87,17 +76,13 @@ public class VraagController : ControllerBase  {
 
   [HttpDelete]
   [Route("delete/{id}")]
-  public async Task<IActionResult> Delete(Guid id)
-  {
+  public async Task<IActionResult> Delete(Guid id) {
     var success = await _vraagRepository.DeleteAsync(id);
-    if (!success)
-    {
+    if (!success) {
       return NotFound();
     }
+
     return Ok("Vraag is succsesvol verwijderd");
   }
-
-  
-  
 
 }
