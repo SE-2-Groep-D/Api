8056b36eb1512af92ab78f6910d9056958965545
Jason <22098186@student.hhs.nl>
2024-01-02 16:28:17 +0100

    bedrijf heeft nu straat en nummer

--- api/Models/DTO/Gebruiker/BedrijfDto.cs
+++ api/Models/DTO/Gebruiker/BedrijfDto.cs
@@ -5,6 +5,7 @@ public class BedrijfsDetails : GebruikerDetails {
   public string Bedrijfsnaam { get; set; }
   public string Postcode { get; set; }
   public string Plaats { get; set; }
+  public string Straat { get; set; }
   public string Nummer { get; set; }
   public string WebsiteUrl { get; set; }
   public string Omschrijving { get; set; }
