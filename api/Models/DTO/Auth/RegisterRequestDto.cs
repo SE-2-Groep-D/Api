cca74297fbfe5e01348b0b77f638d246834008f0
Jason <22098186@student.hhs.nl>
2024-01-03 14:35:30 +0100

    rol mag null zijn bij registreren

--- api/Models/DTO/Auth/RegisterRequestDto.cs
+++ api/Models/DTO/Auth/RegisterRequestDto.cs
@@ -5,6 +5,6 @@
     public bool? GoogleAccount { get; set; } = false;
     public string Email { get; set; }
     public string Password { get; set; }
-    public string[] Roles { get; set; }
+    public string[]? Roles { get; set; }
   }
 }
