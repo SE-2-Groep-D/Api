b9ba0cd763754a209c0610f2970ff70e959048cb
Jason <jason256709@outlook.com>
2024-01-12 18:09:24 +0100

    Modelvalidation Ervaringsdeskundige

--- api/Models/DTO/Auth/request/RegisterErvaringsdeskundigeRequestDto.cs
+++ api/Models/DTO/Auth/request/RegisterErvaringsdeskundigeRequestDto.cs
@@ -1,9 +1,16 @@
-﻿namespace Api.Models.DTO.Auth.request; 
-public class RegisterErvaringsdeskundigeRequestDto : RegisterRequestDto {
+﻿namespace Api.Models.DTO.Auth.request;
+using Api.CustomActionFilters.CustomAttributes;
+using System.ComponentModel.DataAnnotations;
 
+public class RegisterErvaringsdeskundigeRequestDto : RegisterRequestDto {
+    [Required]
+    [Postcode]
   public string Postcode { get; set; }
+    [Required]
   public bool ToestemmingBenadering { get; set; }
+    [Required]
   public string Leeftijdscategorie { get; set; }
+    [Required]
   public string[]? NieuweHulpmiddelen { get; set; }
 
 }
