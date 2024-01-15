2193bffcc2aad5f55ccaad2cca669c16600af421
MaxHB01 <maxboekenstijn@outlook.com>
2024-01-15 15:18:38 +0100

    Revert "Revert "chatresponsedto""
    This reverts commit fd75a1f1f1098253cf73cc70917d4defacdb456e.

---
+++ api/Models/DTO/Bericht/ChatResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using Api.Models.Domain.Bericht;
+
+namespace Api.Models.DTO.Berichten {
+  public class ChatResponseDto {
+    public Guid OtherUserId { get; set; }
+    public Bericht LastMessage { get; set; }
+    public int TotalMessages { get; set; }
+
+  }
+}
