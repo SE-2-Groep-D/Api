1d72ae2ea4d29d51b4fe14b9484d9a97525b58c2
Jason <jason256709@outlook.com>
2024-01-18 17:12:42 +0100

    nog meer modelvalidations

--- api/Models/DTO/Onderzoek/UpdateOnderzoekRequestDto.cs
+++ api/Models/DTO/Onderzoek/UpdateOnderzoekRequestDto.cs
@@ -1,4 +1,7 @@
-﻿namespace Api.Models.DTO.Onderzoek;
+﻿using Api.CustomActionFilters.CustomAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models.DTO.Onderzoek;
 public class UpdateOnderzoekRequestDto {
 
 
@@ -7,14 +10,14 @@ public class UpdateOnderzoekRequestDto {
   public string? Titel { get; set; }
 
   public string? AantalParticipanten { get; set; }
-
-
+  [Url]
   public string? websiteUrl { get; set; }
   public DateTime? StartDatum { get; set; }
   public string? Omschrijving { get; set; }
   public double? Vergoeding { get; set; }
   public string? Locatie { get; set; }
   public string? Type { get; set; }
+  [AllowedValues("open", "active", "ended")]
   public string? Status { get; set; }
 
 }
